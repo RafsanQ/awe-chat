0f4098ddad78d3bb9e8aa3501c053a07be06957f
RafsanQ <rafsan.quayes@gmail.com>
2024-11-30 19:20:23 +0600

    added redirection logic for home page

--- client/app/loading.tsx
+++ client/app/loading.tsx
@@ -1,4 +1,8 @@
 import { Loader2 } from "lucide-react";
 export default function Loading() {
-  return <Loader2 className="animate-spin" />;
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <Loader2 className="animate-spin" />
+    </div>
+  );
 }
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,4 +1,14 @@
+import { getCookie } from "cookies-next";
+import { redirect } from "next/navigation";
+
 export default function RootComponent() {
+  const jwt = getCookie("jwt");
+  if (jwt) {
+    redirect("/chat");
+  } else {
+    redirect("/auth/login");
+  }
+
   return (
     <div className="flex flex-row flex-grow justify-center items-center">
       Home Page
